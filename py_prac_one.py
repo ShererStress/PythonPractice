3597c5d6503ccea1518c686beab29398ab883c6a
ShererStress <ryanswhitehill@gmail.com>
2019-06-30 15:30:57 -0600

    Through exercise 12

---
+++ py_prac_one.py
@@ -0,0 +1,11 @@
+print("Hello World!");
+haveInput = False;
+# while !haveInput
+try:
+    age = int(input("How old are you? "));
+except ValueError:
+    print("That's not an integer");
+    age = "NOPE"
+except:
+    print("You broke it somehow")
+print(age);
---
+++ py_prac_two.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+print(argv)
+
+script = argv[0];
+first = argv[1];
+second = argv[2];
+third = argv[3];
+
+
+print(first)
+print(second)
+print(third)
